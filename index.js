35b3fd95bdb00c23b224b7ec0bb1117e9585ba94
Will Nelson <will@pleb.xyz>
2018-10-08 09:35:06 -0700

    critical feature additions and testing improvements

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-module.exports = (timeZoneOffset = 0) => {
+module.exports = (options = 0) => {
+	if (typeof options === 'object' && options !== null && options.valueOf) options = options.valueOf();
+	if (typeof options === 'string') return options.toLowerCase() === 'tuesday';
+
+	if (typeof options !== 'number') options = Number(options);
+	if (isNaN(options)) options = 0;
+
 	const now = new Date();
-	now.setUTCHours(timeZoneOffset);
+	now.setUTCHours(options);
 	return now.getDay() === 2;
 };
